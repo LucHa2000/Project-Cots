54e3623ef672f454a4ee8e44e557a14f023aca7f
LucHa2000 <danchoiphonui27@gmail.com>
2021-12-02 00:47:04 +0700

    fix ui

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const route = require("./routes");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const server = require("http").Server(app);
 var io = require("socket.io")(server);
 const nodemailer = require("nodemailer");
@@ -19,21 +19,21 @@ require("dotenv").config();
 //parse application/s-www-form-urlencoded
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 app.use(cookieParser()); // use cookie
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 180 * 60 * 1000,
-      secure: false,
-    },
-  })
+    session({
+        secret: "keyboard cat",
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 180 * 60 * 1000,
+            secure: false,
+        },
+    })
 );
 app.use(flash());
 app.use(morgan("combined"));
@@ -42,50 +42,50 @@ app.use(express.json());
 //template Engine
 //app.engine('hbs',exphbs({extname: '.hbs'}))
 app.engine(
-  "hbs",
-  exphbs({
-    extname: ".hbs", //config hbs
-    helpers: require("./helpers/hbs"),
-  })
+    "hbs",
+    exphbs({
+        extname: ".hbs", //config hbs
+        helpers: require("./helpers/hbs"),
+    })
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 //socket io in server
 server.listen(port, () => {
-  console.log(` Server run at http://localhost:${port}`);
+    console.log(` Server run at http://localhost:${port}`);
 });
 const arrayUser = [];
 const usersId = [];
 //check connect
 io.on("connection", (socket) => {
-  console.log("have a connect ID :" + socket.id);
-  socket.on("disconnect", () => {
-    console.log(socket.id + " disconnected !");
-  });
-  // listening  username
-  socket.on("user-name", (data) => {
-    arrayUser.push(data);
-    socket.Username = data;
-    usersId[data] = socket.id;
-    socket.emit("Server-success-regsiter", data);
-  });
-  //listening content
-  socket.on("content-message", (data) => {
-    var socketId = usersId[data.receiver];
-    io.to(socketId).emit("new-message-private", data);
-  });
-  //listening emotion
-  socket.on("content-emotion", (data) => {
-    var socketId = usersId[data.receiver];
-    io.to(socketId).emit("new-message-private-emotion", data);
-  });
-  //listening user write
-  socket.on("user-writing", (data) => {
-    socket.broadcast.emit("server-send-user-write", data);
-  });
-  //stop listening user write
-  socket.on("user-write-stop", (data) => {
-    io.sockets.emit("server-send-user-write-stop");
-  });
+    console.log("have a connect ID :" + socket.id);
+    socket.on("disconnect", () => {
+        console.log(socket.id + " disconnected !");
+    });
+    // listening  username
+    socket.on("user-name", (data) => {
+        arrayUser.push(data);
+        socket.Username = data;
+        usersId[data] = socket.id;
+        socket.emit("Server-success-regsiter", data);
+    });
+    //listening content
+    socket.on("content-message", (data) => {
+        var socketId = usersId[data.receiver];
+        io.to(socketId).emit("new-message-private", data);
+    });
+    //listening emotion
+    socket.on("content-emotion", (data) => {
+        var socketId = usersId[data.receiver];
+        io.to(socketId).emit("new-message-private-emotion", data);
+    });
+    //listening user write
+    socket.on("user-writing", (data) => {
+        socket.broadcast.emit("server-send-user-write", data);
+    });
+    //stop listening user write
+    socket.on("user-write-stop", (data) => {
+        io.sockets.emit("server-send-user-write-stop");
+    });
 });
 route(app);
--- src/public/script/inbox.js
+++ src/public/script/inbox.js
@@ -1,115 +1,99 @@
-var socket = io("http://localhost:5000");
+var socket = io("http://localhost:4000");
 
 var receiver = "";
 var sender = "";
 //listening private emotion
 socket.on("new-message-private-emotion", (data) => {
-  $(".container-chat").show();
-  receiver = data.sender;
-  $("#chat-content").append(
-    // '<h6 class="nameUser">' +
-    //   data.sender +
-    //   ":" +
-    //   '<img class="image-infor-focus" src=' +
-    //   data.message +
-    //   "></h6>"
-
-    '<div class="message-container-receive"><img class="image-infor-focus" src ="' +
-      data.message +
-      '"><h6 class="nameUser">' +
-      data.sender +
-      "</h6></div>"
-  );
+    $(".container-chat").show();
+    receiver = data.sender;
+    $("#chat-content").append(
+        '<div class="message-container-receive"><img class="image-infor-focus text-message message-icon" src ="' +
+        data.message +
+        '"><h6 class="nameUser">' +
+        data.sender +
+        "</h6></div>"
+    );
 });
 //listening private message
 socket.on("new-message-private", (data) => {
-  $(".container-chat").show();
-  receiver = data.sender;
-  $("#chat-content").append(
-    '<div class="message-container-receive"><p class="text-message>' +
-      data.message +
-      '">' +
-      '<h6 class="nameUser">' +
-      data.sender +
-      "</h6></div>"
-  );
-  //listening focus
-  socket.on("server-send-user-write", (data) => {
-    $("#notification-focus").html(
-      data + ": " + "<div class='dot-flashing'></div>"
+    $(".container-chat").show();
+    receiver = data.sender;
+    $("#chat-content").append(
+        '<div class="message-container-receive"><h1></h1>' +
+        data.message +
+        '<h6 class="nameUser">' +
+        data.sender +
+        "</h6></div>"
     );
-  });
-  //stop focus
-  socket.on("server-send-user-write-stop", () => {
-    $("#notification-focus").html("");
-  });
+    //listening focus
+    socket.on("server-send-user-write", (data) => {
+        $("#notification-focus").html(
+            data + ": " + "<div class='dot-flashing'></div>"
+        );
+    });
+    //stop focus
+    socket.on("server-send-user-write-stop", () => {
+        $("#notification-focus").html("");
+    });
 });
 $(document).ready(() => {
-  sender = $("#username").text();
-  socket.emit("user-name", $("#username").text());
-  $(".container-chat").hide();
-  //close box chat when reload
-  $(".collection-item").click(function (event) {
-    receiver = $(this).text();
-    socket.emit("revider", $(this).text());
-    $(".container-chat").show();
-  });
-  //close box chat
-  $("#close-boxchat").click(() => {
+    sender = $("#username").text();
+    socket.emit("user-name", $("#username").text());
     $(".container-chat").hide();
-  });
-
-  //send message
-  $("#send-message").click(() => {
-    var message = $("#message-inbox").val();
-    socket.emit("content-message", {
-      message: message,
-      receiver: receiver,
-      sender: sender,
+    //close box chat when reload
+    $(".collection-item").click(function(event) {
+        receiver = $(this).text();
+        socket.emit("revider", $(this).text());
+        $(".container-chat").show();
     });
-    $("#chat-content").append(
-      '<div class="message-container"><img class="image-infor-focus text-message message-icon"> src="' +
-        message +
-        '">' +
-        '<h6 class="nameUser">' +
-        "You</h6></div>"
-    );
-    $("#message-inbox").val(" ");
-
-    // scrollTop when send message
-    $("#chat-content").animate({ scrollTop: 10000 }, "slow");
-    $("#notification-focus").css("bottom: 0;");
-  });
-  // focusout input , when sender writing
-  $("#message-inbox").focusin(() => {
-    socket.emit("user-writing", sender);
-  });
-  // stop focusout input
-  $("#message-inbox").focusout(() => {
-    socket.emit("user-write-stop");
-  });
-  //
-
-  $(".image-emotion").click(function (event) {
-    var message = $(this).attr("src");
-    socket.emit("content-emotion", {
-      message: message,
-      receiver: receiver,
-      sender: sender,
+    //close box chat
+    $("#close-boxchat").click(() => {
+        $(".container-chat").hide();
     });
-    $("#chat-content").append(
-      // '<h6 class="nameUser">' +
-      //   "Me" +
-      //   ":" +
-      //   '<img class="image-infor-focus" src=' +
-      //   message +
-      //   "></h6>"
-      '<div class="message-container"><img class="image-infor-focus text-message message-icon"> src="' +
-        message +
-        '">' +
-        '<h6 class="nameUser">' +
-        "You</h6></div>"
-    );
-    // $("#modal1").hide()
-  });
+
+    //send message
+    $("#send-message").click(() => {
+        var message = $("#message-inbox").val();
+        socket.emit("content-message", {
+            message: message,
+            receiver: receiver,
+            sender: sender,
+        });
+        $("#chat-content").append(
+            '<div class="message-container"><h1></h1>' +
+            message +
+            '<h6 class="nameUser">' +
+            "You</h6></div>"
+        );
+        $("#message-inbox").val(" ");
+
+        // scrollTop when send message
+        $("#chat-content").animate({ scrollTop: 10000 }, "slow");
+        $("#notification-focus").css("bottom: 0;");
+    });
+    // focusout input , when sender writing
+    $("#message-inbox").focusin(() => {
+        socket.emit("user-writing", sender);
+    });
+    // stop focusout input
+    $("#message-inbox").focusout(() => {
+        socket.emit("user-write-stop");
+    });
+    //
+
+    $(".image-emotion").click(function(event) {
+        var message = $(this).attr("src");
+        socket.emit("content-emotion", {
+            message: message,
+            receiver: receiver,
+            sender: sender,
+        });
+        $("#chat-content").append(
+            '<div class="message-container"><img class="image-infor-focus text-message message-icon" src="' +
+            message +
+            '">' +
+            '<h6 class="nameUser">' +
+            "You</h6></div>"
+        );
+    });
 });
