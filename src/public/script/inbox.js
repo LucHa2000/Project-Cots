624e4d582187afc05a873563b5c7490bbc0a1a72
Nhan Dang <nhan.icdi@gmail.com>
2021-12-01 22:18:17 +0700

    fixed Chat UI and feature

--- src/public/script/inbox.js
+++ src/public/script/inbox.js
@@ -1,73 +1,115 @@
+var socket = io("http://localhost:5000");
 
-   var socket = io("http://localhost:5000")
- 
-   var receiver = ""
-   var sender = ""
-   //listening private emotion
-   socket.on("new-message-private-emotion",(data)=>{
-    $(".container-chat").show()
-    receiver =  data.sender
-    $('#chat-content').append('<h6 class="nameUser">'+data.sender+':'+ '<img class="image-infor-focus" src=' + data.message +'></h6>')
-   })
-   //listening private message
-   socket.on("new-message-private",(data)=>{
-  
-    $(".container-chat").show()
-    receiver =  data.sender
-    $('#chat-content').append('<h6 class="nameUser">'+ data.sender+':'+'<span class="text-message">'+ data.message+'</span></h6>')
-    //listening focus
-    socket.on('server-send-user-write',(data)=>{
-      $('#notification-focus').html(data +": "+"<img class='image-infor-focus' src='../img/img.gif' >")
-  })
-     //stop focus
-     socket.on('server-send-user-write-stop',()=>{
-      $('#notification-focus').html('')
-  })
-  })
-   $(document).ready(()=>{
-        sender = $("#username").text()
-        socket.emit("user-name",$("#username").text())
-        $(".container-chat").hide()
-        //close box chat when reload
-          $(".collection-item").click(function(event){
-              receiver =$(this).text()
-            socket.emit("revider",$(this).text())
-                 $(".container-chat").show()
-          })
-          //close box chat
-          $("#close-boxchat").click(()=>{
-              $(".container-chat").hide()
-          })
-          //send message 
-          $('#send-message').click(()=>{
-            var message =$('#message-inbox').val()
-            socket.emit("content-message",{ 
-              message  : message,
-              receiver  : receiver,
-              sender : sender
-            })
-            $('#chat-content').append('<h6 class="nameUser">'+"Me"+':'+'<span class="text-message">'+ message+'</span></h6>')
-            $('#message-inbox').val(" ") 
-            })
-            // focusout input , when sender writing 
-            $('#message-inbox').focusin(()=>{   
-              socket.emit('user-writing', sender)
-          })
-           // stop focusout input
-          $('#message-inbox').focusout(()=>{
-            socket.emit('user-write-stop')
-        })
-        //
-        
-          $(".image-emotion").click(function(event){
-           var message  = $(this).attr('src')
-           socket.emit("content-emotion",{ 
-             message  : message,
-             receiver  : receiver,
-             sender : sender
-           })
-           $('#chat-content').append('<h6 class="nameUser">'+"Me"+':'+ '<img class="image-infor-focus" src=' + message +'></h6>')
-            // $("#modal1").hide()
-        })
-        })
-     
+var receiver = "";
+var sender = "";
+//listening private emotion
+socket.on("new-message-private-emotion", (data) => {
+  $(".container-chat").show();
+  receiver = data.sender;
+  $("#chat-content").append(
+    // '<h6 class="nameUser">' +
+    //   data.sender +
+    //   ":" +
+    //   '<img class="image-infor-focus" src=' +
+    //   data.message +
+    //   "></h6>"
+
+    '<div class="message-container-receive"><img class="image-infor-focus" src ="' +
+      data.message +
+      '"><h6 class="nameUser">' +
+      data.sender +
+      "</h6></div>"
+  );
+});
+//listening private message
+socket.on("new-message-private", (data) => {
+  $(".container-chat").show();
+  receiver = data.sender;
+  $("#chat-content").append(
+    '<div class="message-container-receive"><p class="text-message>' +
+      data.message +
+      '">' +
+      '<h6 class="nameUser">' +
+      data.sender +
+      "</h6></div>"
+  );
+  //listening focus
+  socket.on("server-send-user-write", (data) => {
+    $("#notification-focus").html(
+      data + ": " + "<div class='dot-flashing'></div>"
+    );
+  });
+  //stop focus
+  socket.on("server-send-user-write-stop", () => {
+    $("#notification-focus").html("");
+  });
+});
+$(document).ready(() => {
+  sender = $("#username").text();
+  socket.emit("user-name", $("#username").text());
+  $(".container-chat").hide();
+  //close box chat when reload
+  $(".collection-item").click(function (event) {
+    receiver = $(this).text();
+    socket.emit("revider", $(this).text());
+    $(".container-chat").show();
+  });
+  //close box chat
+  $("#close-boxchat").click(() => {
+    $(".container-chat").hide();
+  });
+
+  //send message
+  $("#send-message").click(() => {
+    var message = $("#message-inbox").val();
+    socket.emit("content-message", {
+      message: message,
+      receiver: receiver,
+      sender: sender,
+    });
+    $("#chat-content").append(
+      '<div class="message-container"><img class="image-infor-focus text-message message-icon"> src="' +
+        message +
+        '">' +
+        '<h6 class="nameUser">' +
+        "You</h6></div>"
+    );
+    $("#message-inbox").val(" ");
+
+    // scrollTop when send message
+    $("#chat-content").animate({ scrollTop: 10000 }, "slow");
+    $("#notification-focus").css("bottom: 0;");
+  });
+  // focusout input , when sender writing
+  $("#message-inbox").focusin(() => {
+    socket.emit("user-writing", sender);
+  });
+  // stop focusout input
+  $("#message-inbox").focusout(() => {
+    socket.emit("user-write-stop");
+  });
+  //
+
+  $(".image-emotion").click(function (event) {
+    var message = $(this).attr("src");
+    socket.emit("content-emotion", {
+      message: message,
+      receiver: receiver,
+      sender: sender,
+    });
+    $("#chat-content").append(
+      // '<h6 class="nameUser">' +
+      //   "Me" +
+      //   ":" +
+      //   '<img class="image-infor-focus" src=' +
+      //   message +
+      //   "></h6>"
+      '<div class="message-container"><img class="image-infor-focus text-message message-icon"> src="' +
+        message +
+        '">' +
+        '<h6 class="nameUser">' +
+        "You</h6></div>"
+    );
+    // $("#modal1").hide()
+  });
+});
