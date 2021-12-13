d8be893a43287c60f0d0809c9f075ca16a37d2f1
Nhan Dang <nhan.icdi@gmail.com>
2021-12-13 17:14:41 +0700

    added account management (admin)

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../../config/db')
-const Account = require('../models/Account')
+const express = require("express");
+const router = express.Router();
+const db = require("../../config/db");
+const Account = require("../models/Account");
 class AdminController {
-     index(req, res, next){
-        res.render('admin/account_view')
-    }
-   
+  index(req, res, next) {
+    db.execute(Account.findAll(), (err, result) => {
+      if (err) throw err;
+      res.render("admin/account_view", {
+        accounts: result,
+      });
+    });
+  }
 }
-module.exports = new AdminController()
+module.exports = new AdminController();
